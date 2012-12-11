1d189dad76ef27387e62a009ead1a739c0d27fc9
gitjaf <julianfarina@gmail.com>
2012-12-10 21:24:03 -0300

    Ver 0.1.5: - Se agrego la funcionalidad de estructurar los links concatenandolos con 2 strings adicionales anterior y posterior. Esta decision surge para solucionar un problema particular del sistema de tareas y debe ser revisada antes de agregarse a la rama estable.

--- HAL-Collections/grails-app/services/org/sigma/code/plugins/HalCollectionBuilderService.groovy
+++ HAL-Collections/grails-app/services/org/sigma/code/plugins/HalCollectionBuilderService.groovy
@@ -19,7 +19,7 @@ class HalCollectionBuilderService {
 
 	def excludedParams = ['action', 'controller']
 
-	def buildRepresentation = {model, String httpMethod, queryParams ->
+	def buildRepresentation = {model, String httpMethod, queryParams, linksParams ->
 		
 		def representation = new HashMap()
 
@@ -37,7 +37,7 @@ class HalCollectionBuilderService {
 
 		queryParams.page = (queryParams.page ? queryParams.page : page)
 
-		total = model.size
+		total = model.size()
 
 		lastPage = (new BigDecimal((total / itemsPerPage), new MathContext(1, RoundingMode.DOWN))) as Integer
 
@@ -47,17 +47,17 @@ class HalCollectionBuilderService {
 
 		representation.data.total = total
 
-		links.self = ['href' : this.getLinkTo(controller, action, queryParams)]
+		links.self = ['href' : this.getLinkTo(controller, action, queryParams, linksParams)]
 
-		representation._links = this.getNavigationLinks(controller, action, links, queryParams)
+		representation._links = this.getNavigationLinks(controller, action, links, queryParams, linksParams)
 
-		representation._embedded = this.getEmbedded(queryParams, model)
+		representation._embedded = this.getEmbedded(queryParams, model, linksParams)
 
 		return representation
 
 	}
 
-	protected getLinkTo = { String controller, String action, queryParams ->
+	protected getLinkTo = { String controller, String action, queryParams, linksParams ->
 
 		def query = ''
 
@@ -68,35 +68,35 @@ class HalCollectionBuilderService {
 			query = "?" + query.join("&")
 		}
 
-		return (halBuilderService.getLinkTo(controller, action) + query)
+		return (halBuilderService.getLinkTo(controller, action, linksParams) + query)
 
 	}
 
 
-	protected getNavigationLinks = {String controller, String action, HashMap links, queryParams -> 
+	protected getNavigationLinks = {String controller, String action, HashMap links, queryParams, linksParams -> 
 		
 		if(page > 0){
 			links."previous" = ['href' : this.getLinkTo(controller, action, queryParams.inject([:]) { qp, k, v ->
-				qp << ("$k" == "page" ? ["$k":(Math.max((page - 1),0))] : ["$k":v] )})]
+				qp << ("$k" == "page" ? ["$k":(Math.max((page - 1),0))] : ["$k":v] )}, linksParams)]
 		}
 		
 		links.pages = ((Math.max(page - visiblePages, 0))..(Math.min(page + visiblePages, lastPage))).
 			collectEntries { it ->
 				["$it", ['href' : this.getLinkTo(controller, action, queryParams.inject([:]){qp, k, v ->
-					qp << ("$k" == "page" ? ["$k" : it] : ["$k":v] )})]]
+					qp << ("$k" == "page" ? ["$k" : it] : ["$k":v] )}, linksParams)]]
 			}
 	
 
 		if(page < lastPage) {
 			links."next" = ['href': this.getLinkTo(controller, action, queryParams.inject([:]) { qp, k, v ->
-				qp << ("$k" == "page" ? ["$k": Math.min((page + 1), lastPage)] : ["$k":v]) })]
+				qp << ("$k" == "page" ? ["$k": Math.min((page + 1), lastPage)] : ["$k":v]) }, linksParams)]
 		}
 
 		return links
 	}
 	
 
-	protected getEmbedded = { queryParams, model -> 
+	protected getEmbedded = { queryParams, model, linksParams -> 
 		if(total != 0){
 
 			def offset = itemsPerPage * (Math.min(page, lastPage)) // Starting item of the page
@@ -105,7 +105,7 @@ class HalCollectionBuilderService {
 
 			def tot = Math.max(total - 1, 0) // Max quantity of items in the last page
 
-			return ["collection" : halBuilderService.buildModelList(model[offset..Math.min(max, tot)])]
+			return ["collection" : halBuilderService.buildModelList(model[offset..Math.min(max, tot)], linksParams)]
 		} 
 
 		return ["collection" : [:]]
